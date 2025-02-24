ca8c67d2347ed0d36c0e7a4ff12bd140326f12bc
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-25 01:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your JS code here. If required.
+let books=[];
+
+document.getElementById("submit").addEventListener('click', function(){
+	let title=document.getElementById('title').value;
+	let author=document.getElementById('author').value;
+	let isbn=document.getElementById('isbn').value;
+
+	books.push({title,author,isbn});
+
+	showTable();
+
+	// Clear input fields
+    document.getElementById('title').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('isbn').value = "";
+});
+
+function showTable() {
+	let tableBody = document.getElementById('book-list');
+	tableBody.innerHTML = "";
+	for(let book of books){
+		tableBody.innerHTML += `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+			<td><button onclick="handleDelete('${book.title}')">Delete</button></td>
+        </tr>
+    `;
+	}
+}
+
+function  handleDelete(title) {
+	books=books.filter(book=>book.title!=title);
+	showTable();
+}

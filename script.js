7b9628253f0020087a249e7f3775055c9649505b
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-04 01:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,71 @@
-let books=[];
+// let books=[];
+
+// document.getElementById("submit").addEventListener('click', function(){
+// 	let title=document.getElementById('title').value;
+// 	let author=document.getElementById('author').value;
+// 	let isbn=document.getElementById('isbn').value;
+
+// 	books.push({title,author,isbn});
+
+// 	showTable();
+
+// 	// Clear input fields
+//     document.getElementById('title').value = "";
+//     document.getElementById('author').value = "";
+//     document.getElementById('isbn').value = "";
+// });
+
+// function showTable() {
+// 	let tableBody = document.getElementById('book-list');
+// 	tableBody.innerHTML = "";
+// 	for(let book of books){
+// 		tableBody.innerHTML += `
+//         <tr>
+//             <td>${book.title}</td>
+//             <td>${book.author}</td>
+//             <td>${book.isbn}</td>
+// 			<td><button class="delete" onclick="handleDelete('${book.title}')">Delete</button></td>
+//         </tr>
+//     `;
+// 	}
+// }
+
+// function  handleDelete(title) {
+// 	books=books.filter(book=>book.title!=title);
+// 	showTable();
+// }
+
+let books = [];
 
 document.getElementById("submit").addEventListener('click', function(){
-	let title=document.getElementById('title').value;
-	let author=document.getElementById('author').value;
-	let isbn=document.getElementById('isbn').value;
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbn = document.getElementById("isbn").value;
 
-	books.push({title,author,isbn});
+	books.push({title, author, isbn});
 
-	showTable();
+	displayTable(books);
 
-	// Clear input fields
-    document.getElementById('title').value = "";
-    document.getElementById('author').value = "";
-    document.getElementById('isbn').value = "";
+	document.getElementById("title").value="";
+	document.getElementById("author").value="";
+	document.getElementById("isbn").value="";
 });
 
-function showTable() {
-	let tableBody = document.getElementById('book-list');
-	tableBody.innerHTML = "";
-	for(let book of books){
-		tableBody.innerHTML += `
-        <tr>
-            <td>${book.title}</td>
-            <td>${book.author}</td>
-            <td>${book.isbn}</td>
-			<td><button class="delete" onclick="handleDelete('${book.title}')">Delete</button></td>
-        </tr>
-    `;
-	}
+function displayTable(books){
+	document.getElementById("book-list").innerHTML="";
+	books.map((book, index)=>{
+		document.getElementById("book-list").innerHTML+=`
+		<tr>
+		<td>${book.title}</td>
+		<td>${book.author}</td>
+		<td>${book.isbn}</td>
+		<td><button class="delete" onclick=handleDelete('${book.title}')>Delete</button></td>
+		</tr>
+		`
+	})	
 }
 
-function  handleDelete(title) {
-	books=books.filter(book=>book.title!=title);
-	showTable();
+function handleDelete(title){
+	books = books.filter((book) => {return book.title!=title});
+	displayTable(books);
 }
